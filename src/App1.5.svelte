c1dfa34715f6d07b528da0f407acded9795ffc84
0x333333333 <69386697+0x333333333@users.noreply.github.com>
2023-04-10 17:28:54 +0800

    1 file modified, 1 added

--- src/App1.5.svelte
+++ src/App1.5.svelte
@@ -2,36 +2,39 @@
 <script>
   import { onMount } from 'svelte';
   import { scale } from 'svelte/transition';
-  let pools = [];
-  let eth_pools = [];
+  let wbnb_pools = [];
+  let weth_pools = [];
   
-  async function fetchBNB() {
-    const response = await fetch('https://api.geckoterminal.com/api/v2/networks/bsc/tokens/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c/pools');
-    const data = await response.json();
-    pools = data.data.filter(pool => pool.attributes.name.includes('WBNB'));
+async function fetchData() {
+  const bnb_response = await fetch('https://api.geckoterminal.com/api/v2/networks/bsc/tokens/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c/pools');
+  const eth_response = await fetch('https://api.geckoterminal.com/api/v2/networks/eth/tokens/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/pools');
+  const bnb_data = await bnb_response.json();
+  const eth_data = await eth_response.json();
+
   }
-  // async function fetchETH() {
-  //   const response = await fetch('https://api.geckoterminal.com/api/v2/networks/eth/tokens/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/pools');
-  //   const data = await response.json();
-  //   eth_pools = data.data.filter(pool => pool.attributes.name.includes('WETH'));
-  // }
   
   onMount(() => {
-    fetchBNB();
-    fetchETH();
-    setInterval(fetchBNB, 60000);
+    fetchData();
+    setInterval(fetchData, 100000);
   });
-  
+
+  let eth = false;
+  let bnb = false;
+  let arb = false;
+
 </script>
+
+
+
 
 <style>
   img{
-    height: 3em;
+    height: 4em;
   }
 
   h1 {
     text-align: center;
-    font-size: 3em;
+    font-size: 2.5em;
     margin-bottom: 14px;
     font-family: 'Nunito Sans';
   }
@@ -63,7 +66,9 @@
     text-decoration: none;
     font-family: 'Nunito Sans';
   }
-
+  .eth{
+    background-color: #8c8c8c;
+  }
   a:hover {
     text-decoration: underline;
   }
@@ -85,10 +90,11 @@
   }
 </style>
 
-<!-- <img in:scale src="https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=024" alt="ETH Logo" class="bnb-logo" />
-<h1>$ETH DEFI VALUE</h1> -->
+
+<div on:click={()=>{eth=false;bnb=true;}}>
 <img in:scale src="https://cryptologos.cc/logos/bnb-bnb-logo.svg?v=024" alt="BNB Logo" class="bnb-logo" />
-<h1>BNB DEFI VALUE</h1>
+<h1>$BNB DEFI VALUE</h1>
+</div>
 
 <table>
   <thead in:scale>
@@ -100,7 +106,7 @@
     </tr>
   </thead>
   <tbody in:scale>
-    {#each pools as pool}
+    {#each wbnb_pools as pool}
       {#if pool.attributes.name.split(' / ')[1] === 'WBNB'}
         <tr>
           <td>{pool.attributes.name.split('/')[1]}/{pool.attributes.name.split('/')[0]}</td>
@@ -119,3 +125,41 @@
     {/each}
   </tbody>
 </table>
+
+<br>
+<div on:click={()=>{eth=true;bnb=false;}}>
+<img in:scale src="https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=024" alt="ETH Logo" class="bnb-logo" />
+<h1>$ETH DEFI VALUE</h1>
+</div>
+
+<table>
+  <thead in:scale>
+    <tr>
+      <th class="eth">POOL LP</th>
+      <th class="eth">SWAPPING POOL ADDRESS</th>
+      <th class="eth">WETH VALUE（USD）</th>
+      <th class="eth">TOKEN VALUE（USD）</th>
+    </tr>
+  </thead>
+  <tbody in:scale>
+    {#each weth_pools as pool}
+      {#if pool.attributes.name.split(' / ')[1] === 'WETH'}
+        <tr>
+          <td>{pool.attributes.name.split('/')[1]}/{pool.attributes.name.split('/')[0]}</td>
+          <td><a href="https://etherscan.io/address/{pool.attributes.address}" target="_blank">Etherscan</a></td>
+          <td>{pool.attributes.quote_token_price_usd}</td>
+          <td>{pool.attributes.base_token_price_usd}</td>
+        </tr>
+      {:else if pool.attributes.name.split(' / ')[0] === 'WETH'}
+        <tr>
+          <td>{pool.attributes.name.split('/')[0]}/{pool.attributes.name.split('/')[1]}</td>
+          <td><a href="https://etherscan.io/address/{pool.attributes.address}" target="_blank">Etherscan</a></td>
+          <td>{pool.attributes.base_token_price_usd}</td>
+          <td>{pool.attributes.quote_token_price_usd}</td>
+        </tr>
+      {/if}
+    {/each}
+  </tbody>
+</table>
+
+
