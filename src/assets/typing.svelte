a430de885faab33ed69667ec3c33a9726dbbe402
0x333333333 <69386697+0x333333333@users.noreply.github.com>
2023-08-05 15:17:56 +0800

    1 file modified, 2 added

---
+++ src/assets/typing.svelte
@@ -0,0 +1,26 @@
+<!-- AnimatedText.svelte -->
+<script>
+  import { onMount } from 'svelte';
+
+  export let text = '';
+  let currentIndex = 0;
+
+  let typedText = '';
+
+  function typeNextLetter() {
+    if (currentIndex < text.length) {
+      typedText += text[currentIndex];
+      currentIndex += 1;
+      setTimeout(typeNextLetter, 100); // Adjust the typing speed here (e.g., 100ms for 0.1 second delay)
+    }
+  }
+
+  // Use onMount to start typing animation after the component is mounted
+  onMount(() => {
+    typeNextLetter();
+  });
+</script>
+
+<div>
+  {typedText}
+</div>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './app.css'
-import App from './home.svelte'
+import App from './landing.svelte'
 
 const app = new App({
   target: document.getElementById('app'),
